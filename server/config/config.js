82914af22c6a6dde9dc9619909212cca97762882
vbertolini <vbertolini@MacBook-Pro-de-Vittorio.local>
2019-07-28 20:07:29 -0400

    se esconde url

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://style:wExZWpDxQnbzggtE@cluster0-fd7ok.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
